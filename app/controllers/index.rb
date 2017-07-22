e8a4e881f15c1dee043f7eee06429fb853d5cf3c
Ginny Fahs & Tyler Kawabata <pair+ginnyfahs.tylerk64@devbootcamp.com>
2017-07-21 17:17:39 -0700

    Made views for decks and cards, made correct evaluation methods for form.

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,24 @@
 get '/' do
+  @decks = Deck.all
+  erb :'/decks/index'
+end
+
+before '/deck/:deck_id/card/*' do
+ @cards = Card.where({:deck_id => params[:deck_id], :correct_guess => nil})
+ @card = @cards.sample
+end
+
+get '/deck/:deck_id/card/:card_id' do
+  @cards = Card.where(params[:id])
+  @all_cards = Card.all
+  erb :'/decks/show'
+end
+
+post '/guess' do
+  p params
+  @guess = Guess.create({:guess => params[:form_guess], :card_id => params[:card_id]})
+  p @guess.first_correct?
+  p @guess.correct?
+
+  erb :'/decks/show'
 end
--- app/models/card.rb
+++ app/models/card.rb
@@ -2,4 +2,11 @@ class Card < ApplicationRecord
   # Remember to create a migration!
   belongs_to :deck
   has_many :guesses
+
+  # after_initialize :set_defaults
+
+  # private
+  # def set_defaults
+  #   self.correct_guess = false
+  # end
 end
