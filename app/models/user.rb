f97cf2aeec16645fc1c56c09157bbf5b098faaed
EJ Vickery <email": null>
2017-07-21 17:52:17 -0700

    Update register and login paths

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,4 +13,10 @@ class User < ApplicationRecord
     @password = BCrypt::Password.create(new_password)
     self.password_hash = @password
   end
+
+  def self.authenticate(username, password)
+    user = User.find_by({username: username})
+    return true if user.password == password
+    nil
+  end
 end
